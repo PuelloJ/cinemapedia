93efdc5a41a2ebde163565d21aa09d2ec5310896
Jose-01010000 <josepuello112000@gmail.com>
2024-11-23 19:26:49 -0500

    Refactor HomeMovieScreen to include CustomAppbar and MoviesSlideshow for improved UI layout

--- lib/presentation/screens/movies/home_movie_screen.dart
+++ lib/presentation/screens/movies/home_movie_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -33,15 +34,23 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (BuildContext context, int index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-          subtitle: Text(movie.overview),
-        );
-      },
+    return Column(
+      children: [
+        const CustomAppbar(),
+        MoviesSlidesshow(movies: nowPlayingMovies),
+        // Expanded(
+        //   child: ListView.builder(
+        //     itemCount: nowPlayingMovies.length,
+        //     itemBuilder: (BuildContext context, int index) {
+        //       final movie = nowPlayingMovies[index];
+        //       return ListTile(
+        //         title: Text(movie.title),
+        //         subtitle: Text(movie.overview),
+        //       );
+        //     },
+        //   ),
+        // ),
+      ],
     );
   }
 }
