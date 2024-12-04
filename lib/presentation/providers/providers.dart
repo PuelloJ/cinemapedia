5518bfdb95ce1b07e14ebfc773d184dc82b87d43
Jose-01010000 <josepuello112000@gmail.com>
2024-12-04 18:17:40 -0500

    Agregar capa de actores con datasource, repositorio y mapeo de datos

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -1,5 +1,10 @@
+// Movies
 export 'movies/movies_providers.dart';
 export 'movies/movies_repository_provider.dart';
 export 'movies/movies_slidesshow_provider.dart';
 export 'movies/initial_loading_provider.dart';
 export 'movies/movie_info_provider.dart';
+
+// Actors
+export 'actors/actors_by_movie_provider.dart';
+export 'actors/actors_repository_provider.dart';
