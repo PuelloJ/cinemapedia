90eb3f8133a467267521ee820ff9b56955b525d8
Jose-01010000 <josepuello112000@gmail.com>
2024-12-09 16:34:35 -0500

    Agregar implementación de almacenamiento local para gestionar películas favoritas

---
+++ lib/infrastructure/datasources/isar_datasource.dart
@@ -0,0 +1,26 @@
+
+import '../../domain/datasources/local_storage_datasource.dart';
+import '../../domain/entities/movie.dart';
+
+class IsarDatasource extends LocalStorageDatasource {
+  @override
+  Future<bool> isMovieFavorite(int movieId) {
+    // TODO: implement isMovieFavorite
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Movie>> loadMovies({int limit = 10, offset = 0}) {
+    // TODO: implement loadMovies
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> toggleFavorite(Movie movie) {
+    // TODO: implement toggleFavorite
+    throw UnimplementedError();
+  }
+
+
+
+}
