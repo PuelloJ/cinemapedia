36a0a968a4527cb2caaa3ee1e83db7d185826513
Jose-01010000 <josepuello112000@gmail.com>
2024-12-09 14:10:16 -0500

    Actualizar rutas de navegación para incluir el índice de página en las transiciones a detalles de películas y en la navegación inferior

--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
-  const CustomBottomNavigation({super.key});
+  final int pageCurrentIndex;
+
+  const CustomBottomNavigation({super.key, required this.pageCurrentIndex});
+
+  void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/home/0');
+        break;
+      case 1:
+        context.go('/home/1');
+        break;
+      case 2:
+        context.go('/home/2');
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0,
+      currentIndex: pageCurrentIndex,
+      onTap: (index) => onItemTapped(context, index),
       items: const [
         BottomNavigationBarItem(
           icon: Icon(Icons.home_max_outlined),
